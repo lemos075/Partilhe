405e74291209be7cb3f3e601d970f20e169964e0
Felipe <lemosfelipe5164@gmail.com>
2026-08-26 10:43:33 -0300

    added database for the site using mysql and cloudinary

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, } from '@angular/router';
+import { HeaderComponent } from './Components/header/header';
+import { FooterComponent } from './Components/footer/footer';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true, //garante compatibilidade visual com standalone
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('partilhe');
+
 }
